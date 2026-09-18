084a4d0f52368590bd49e5bc812e654d7b8d9a82
bsidesign <muhammad.muzakki@bsi.co.id>
2026-09-18 23:04:05 +0700

    Change Font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Onest:wght@400;500;600;700;800&display=swap"
           rel="stylesheet"
         />
       </head>
